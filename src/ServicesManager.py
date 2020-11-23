97abe2a608082ebf6ef674d52e1517f7ee41ac79
Travis B Hammond <Mountainscaler@ymail.com>
2020-11-22 19:14:19 -0800

    Add files via upload

--- src/ServicesManager.py
+++ src/ServicesManager.py
@@ -1,20 +1,22 @@
 '''
-Technical Lead: Tyler Dabat
-Contributor: Travis Hammond
+Technical Lead: Travis Hammond
+Contributor: Tyler Dabat
 COMP 3006 Final
 '''
 
 import logging
-import DataExtractor_2 as de
+import DataExtractor as de
+import CommodityIndex as ci
 import ServicesIndex as si
-import PlotHelper_2 as ph
-import plotter_2 as ptr
-import printer_2 as pnr
+import PlotHelper as ph
+import Plotter as ptr
+import Printer as pnr
 import numpy as np
 
 
 
-#creating services
+
+#creating services	
 def createServices(service, logger):
 
     #extracting services from source
@@ -29,58 +31,108 @@ def createServices(service, logger):
     logger.debug('Services created')
     return services
 
+#creating commodities
+def createCommodities(commodity, logger):
+    
+    #extracting commodities from source
+    commodityData = de.extractCommoditiesData(commodity, logger)
+    commodities = ci.Commodities(logger)
+    
+    logger.debug('Creating Commodities')
+    #instantiating commodities
+    for row in commodityData.itertuples():
+        commodities.addNewCommodity(row.series_id, commodity, row.year, row.period, row.value, logger)
+        
+    logger.debug('Commodities created')
+    return commodities
 
 #evaluate and display plot
-def displayPlot(services, plotType, logger):
-
+def displayPlot(commodities, services, plotType, logger):
+    
     #flat plot type execution
     if plotType == 'flat':
         logger.debug('flat chosen')
-
-        x,y = ph.createFlatData(services, logger)
-        ptr.plotFlatData(x,y, logger)
+        
+        x,y = ph.createFlatData(commodities, logger)
+        
+        # add conditions to handle Null values
+        x2 = None
+        y2 = None
+        if services != None:
+            x2,y2 = ph.createFlatData(services, logger)
+        
+        #plotting
+        ptr.plotFlatData(x,y,x2,y2, logger)
         logger.debug('flat plotter invoked')
-
-
+        
+        
     #nominal plot type execution
     elif plotType == 'nomchge':
         logger.debug('nomchge chosen')
-
-        x,y = ph.createNomialData(services, logger)
-        ptr.plotNominalData(x,y, logger)
+        
+        x,y = ph.createNomialData(commodities, logger)
+        
+        #handling nulls
+        x2 = None
+        y2 = None
+        if services != None:
+            x2,y2 = ph.createNomialData(services, logger)
+          
+        #plotting  
+        ptr.plotNominalData(x,y,x2,y2, logger)
         logger.debug('Nominal plotter invoked')
-
-    #nominal plot type execution
+        
+    #percentage plot type execution
     elif plotType == 'perchge':
         logger.debug('perchge')
-
-        x,y = ph.createPercentageData(services, logger)
-        ptr.plotPercentageData(x,y, logger)
-
+        
+        x,y = ph.createPercentageData(commodities, logger)
+        
+        #handlingNulls
+        x2 = None
+        y2 = None
+        if services != None:
+            x2,y2 = ph.createPercentageData(services, logger)
+        
+        #plotting
+        ptr.plotPercentageData(x,y,x2,y2, logger)
         logger.debug('Percentage plotter invoked')
-
+        
     #no plot type
     elif plotType == None:
+        x,y = ph.createPercentageData(commodities, logger)
         logger.info("No plot chosen")
         logger.debug("Plot input Null")
-
+        
+        
+    
     #error in plot type
     else:
         logger.error('plot display type invalid, investigation needed')
-
-
+        
+    
     return x,y
 
-#Processing all flows from main
-def processServices(service, plot, outputType, logger):
-    #Creating services
-    services = createServices(service, logger)
-    logger.info(service + ' Obtained')
-
+#Processing all flows from main  (Also, numpy is used here
+def processCommodities(commodity, plot, outputType, grouping,  logger):
+    #Creating commodities
+    commodities = createCommodities(commodity, logger)
+    logger.info(commodity + ' Obtained')
+    
+    
+    services = None
+    
+    #creating services
+    if grouping == True:
+        services = createServices(commodity, logger)
+        logger.info('Service: ' + commodity + 'Obtained')
+    
+    
     #displaying Plots
-    x,y = displayPlot(services, plot, logger)
-    logger.debug(plot + ' attempted')
-
+    x,y = displayPlot(commodities, services, plot, logger)
+    logger.debug(str(plot) + ' attempted')
+    
+    #<<Numpy
     output = [np.column_stack((x,y))]
     #output handled here
     pnr.printOutput(output, outputType, logger)
